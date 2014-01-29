976fdb62033c1e92edcb4abfb29b12c0524313f9
Joakim Gustin <joakim@dynamiccommunication.se>
2014-01-29 16:33:19 +0100

    Logging to io.writer instead

--- request.go
+++ request.go
@@ -1,20 +1,25 @@
 package martiniHandlers
 
 import (
+	"fmt"
 	"github.com/codegangsta/martini"
-	"github.com/coreos/go-log/log"
+	"io"
 	"net/http"
 	"time"
 )
 
+type RequestWriter struct {
+	writer io.Writer
+}
+
 // RequestLogger returns a middleware handler that logs the request as it goes in and the response as it goes out.
-func RequestLogger() martini.Handler {
-	return func(res http.ResponseWriter, req *http.Request, c martini.Context, logger *log.Logger) {
+func RequestLogger(writer io.Writer, resourceName string) martini.Handler {
+	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
 		start := time.Now()
 
 		rw := res.(martini.ResponseWriter)
 		c.Next()
 
-		logger.Infof("[REQ] %v %s %s in %v", rw.Status(), req.Method, req.URL.Path, time.Since(start))
+		writer.Write([]byte(fmt.Sprintf("REQUEST: %s %s%s %v in %v\n", req.Method, resourceName, req.URL.Path, rw.Status(), time.Since(start))))
 	}
 }
